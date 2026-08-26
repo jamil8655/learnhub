74f0afa5ce2e1feb55673c63a3892eb5fe8031fc
jamil8655 <jrahmanansari132@gmail.com>
2026-08-26 20:18:06 +0530

    Auto-Sync: update js/views/v2/coursesV2.js

---
+++ js/views/v2/coursesV2.js
@@ -0,0 +1,11 @@
+/**
+ * LearnHub V2 Modern Course Catalog
+ */
+window.Views = window.Views || {};
+window.Views.v2 = window.Views.v2 || {};
+
+window.Views.v2.renderCourses = function(params, query) {
+  if (typeof window.Views.renderCourses === 'function') {
+    return window.Views.renderCourses(params, query);
+  }
+};
