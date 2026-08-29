19e270953adc56359346c7535f0b67ee073dff5f
jamil8655 <jrahmanansari132@gmail.com>
2026-08-29 09:59:47 +0300

    Auto-Sync: update js/views/v2/quizzesV2.js

--- js/views/v2/quizzesV2.js
+++ js/views/v2/quizzesV2.js
@@ -1,12 +1,31 @@
 /**
- * LearnHub V2 Modern Quiz Arena with Lifelines & Analysis
+ * LearnHub V2 Quiz Proxy
+ * Seamlessly routes to the unified master Quiz Examination & Arena Suite
  */
 
 window.Views = window.Views || {};
 window.Views.v2 = window.Views.v2 || {};
 
+window.Views.v2.renderQuizzes = function(params, query) {
+  if (typeof window.Views.renderQuizzes === 'function') {
+    return window.Views.renderQuizzes(params, query);
+  }
+};
+
+window.Views.v2.renderQuizDetails = function(params, query) {
+  if (typeof window.Views.renderQuizDetails === 'function') {
+    return window.Views.renderQuizDetails(params, query);
+  }
+};
+
+window.Views.v2.renderQuizTake = function(params, query) {
+  if (typeof window.Views.renderQuizTake === 'function') {
+    return window.Views.renderQuizTake(params, query);
+  }
+};
+
 window.Views.v2.renderQuizArena = function(params, query) {
-  if (typeof window.Views.renderAdventureGame === 'function') {
-    return window.Views.renderAdventureGame(params, query);
+  if (typeof window.Views.renderQuizTake === 'function') {
+    return window.Views.renderQuizTake(params, query);
   }
 };
