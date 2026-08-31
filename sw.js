73626a81c73e570ee235a130953e0bad89f023f0
jamil8655 <jrahmanansari132@gmail.com>
2026-08-31 17:58:22 +0300

    Auto-Sync: update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@
  * Robust Offline Caching & Background Resilience
  */
 
-const CACHE_NAME = 'learnhub-static-v183.0.0';
-const RUNTIME_CACHE = 'learnhub-runtime-v183.0.0';
+const CACHE_NAME = 'learnhub-static-v184.0.0';
+const RUNTIME_CACHE = 'learnhub-runtime-v184.0.0';
 
 const STATIC_ASSETS = [
   './',
