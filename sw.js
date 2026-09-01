d8169812b24fd13fe9bbb212534f98c4caf3a113
jamil8655 <jrahmanansari132@gmail.com>
2026-09-01 05:00:52 +0300

    Auto-Sync: update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@
  * Robust Offline Caching & Background Resilience
  */
 
-const CACHE_NAME = 'learnhub-static-v237.0.0';
-const RUNTIME_CACHE = 'learnhub-runtime-v237.0.0';
+const CACHE_NAME = 'learnhub-static-v238.0.0';
+const RUNTIME_CACHE = 'learnhub-runtime-v238.0.0';
 
 const STATIC_ASSETS = [
   './',
@@ -108,7 +108,7 @@ self.addEventListener('install', (event) => {
   self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Pre-caching App Shell v237.0.0');
+      console.log('[ServiceWorker] Pre-caching App Shell v238.0.0');
       return Promise.allSettled(
         STATIC_ASSETS.map(asset => cache.add(asset).catch(e => console.warn('[SW] Cache item skipped:', asset, e)))
       );
